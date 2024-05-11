2247ad6ac1da8bc4325a79ffecb75628bf80c36e
Ernest Subotić <gamedevsubo@gmail.com>
2024-05-11 16:43:40 +0200

    Add ItemPickUp

---
+++ Assets/Scripts/ItemPickUp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    [SerializeField] ItemData item;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if (InventoryManager.Instance.Add(item) == true)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void LaunchInDirection(Vector3 direction, float launchForce = 7f)
+    {
+        // Ensure the object has a Rigidbody component
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            // Apply force in the specified direction
+            rb.AddForce(direction.normalized * launchForce, ForceMode.Impulse);
+            Debug.Log("Launched");
+        }
+        else
+        {
+            Debug.LogWarning("Item does not have a Rigidbody component.");
+        }
+    }
+}
