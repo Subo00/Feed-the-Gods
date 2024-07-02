e91c7add9f499a27b6e9dea41d652fc334bf34b6
Ernest Subotić <gamedevsubo@gmail.com>
2024-07-02 16:54:53 +0200

    Update Source Base to use Interactable as a parent

--- Assets/Scripts/SourceBase.cs
+++ Assets/Scripts/SourceBase.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using System.Threading;
 using System.Runtime.CompilerServices;
 
-public class SourceBase : MonoBehaviour,  IMyUpdate
+public class SourceBase : Interactable
 {
     [System.Serializable]
     public struct ResourceDrop
@@ -16,90 +16,45 @@ public class SourceBase : MonoBehaviour,  IMyUpdate
     }
 
     [SerializeField]
-    List<ResourceDrop> resourceDrops;
+    protected List<ResourceDrop> resourceDrops;
 
     //Interaction variables
-    private float lastInteractionTime;
-    public float interactionCooldown = 6f;
+    protected float lastInteractionTime;
+    [SerializeField] protected float interactionCooldown = 6f;
 
     [SerializeField] private MinigameManager.MinigameType minigameType;
-    [SerializeField] private Transform dropPoint;
-    private bool inMinigame = false;
 
-    private ItemManager itemManager;
-    private UIManager uiManager;
+    protected ItemManager itemManager;
 
-    private void Start()
+    protected override void Start()
     {
         lastInteractionTime = -interactionCooldown;
         itemManager = ItemManager.Instance;
-        uiManager = UIManager.Instance;
+        
+        base.Start();
     }
 
-    void IMyUpdate.MyUpdate()
+    protected override void OnUpdate()
     {
-        if(Time.time - lastInteractionTime < interactionCooldown)
+        if (Time.time - lastInteractionTime < interactionCooldown)
         {
             float difference = interactionCooldown - (Time.time - lastInteractionTime);
             uiManager.ShowTimeOnObject(dropPoint, Mathf.Round(difference * 10) / 10);
         }
-        else if(Input.GetButtonDown("Interact"))
+        else if (Input.GetButtonDown("Interact"))
         {
             MinigameManager.Instance.SetOnFinishMinigame(DropResource);
             MinigameManager.Instance.StartMinigame(minigameType);
-            inMinigame = true;
-        }
-        else if(inMinigame)
-        {
-            uiManager.HideInteraction();
+            inUse = true;
         }
         else
         {
-            uiManager.ShowInteractionOnObject(dropPoint);
+            CommonLogic();
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public virtual void DropResource(float dummy)
     {
-        if (other.CompareTag("PlayerInteraction"))
-        {
-            //Add UI
-            UpdateManager.Instance.AddUpdatable(this);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag("PlayerInteraction"))
-        {
-            //Remove UI 
-            UIManager.Instance.HideInteraction();
-            UpdateManager.Instance.RemoveUpdatable(this);
-        }
-    }
-    public void DropResource(float dummy)
-    {
-        float randNum = Random.Range(0.01f, 1f);
-
-        foreach(var resource in resourceDrops)
-        {
-            if (resource.dropChance < randNum) continue; 
-
-            GameObject itemToDrop = itemManager.GetGameObject(resource.item.id);
-            
-           
-            for(uint i = 0; i < resource.value; i++)
-            {
-                GameObject drop = Instantiate(itemToDrop, dropPoint.position, Quaternion.identity);
-                Vector3 rotation = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
-                drop.GetComponent<ItemPickUp>().LaunchInDirection(rotation);
-                Thread.Sleep(50);
-            }
-            
-        }
-
-        //set a cooldown 
-        lastInteractionTime = Time.time;
-        inMinigame = false;
+       
     }
 }
