79ee42e13221022767a412afbfc3c1344b50aed5
Ernest Subotić <gamedevsubo@gmail.com>
2025-11-07 08:23:24 +0100

    Updated MinigameHeadHit

--- Assets/Managers/Minigames/MinigameHeadHit.cs
+++ Assets/Managers/Minigames/MinigameHeadHit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MinigameHeadHit : Minigame
@@ -6,10 +7,55 @@ public class MinigameHeadHit : Minigame
     [SerializeField] private float speed = 30f;
     [SerializeField] private float botLimit = -190f;
     [SerializeField] private float topLimit = 190f;
-    [SerializeField] private float sweetSpot = 180f;
+    [SerializeField] private float sweetSpot = 170f;
 
     private bool goingDown = false;
 
+
+    public override void StartMinigame(uint valueUint, MinigameManager minigameManager = null)
+    {
+        base.StartMinigame(valueUint, minigameManager);
+
+        manager.Player.ClearInteract();
+        manager.Player.SetInteract(OnInteract);
+        manager.Player.SetCancle(() => manager.EndMinigame(-1f));
+        manager.Player.Animator.PlayAllLayers(Animations.IDLE);
+       // manager.Player.Animator.Play(Animations.STOMP, 2, false, false);
+    }
+
+    public void OnInteract()
+    {
+        float temp = Mathf.Abs(sweetSpot - indicator.anchoredPosition.y);
+        //add audio 
+
+        //add animation
+        Debug.Log("temp: " + temp + "\nindicatorPos: " + indicator.anchoredPosition.y);
+
+        switch(temp)
+        {
+            case <= 10:
+                //manager.EndMinigame(1f);
+                Debug.Log("1f");
+                break;
+            case <= 20:
+                Debug.Log("0.7f");
+
+                //manager.EndMinigame(0.7f);
+                break;
+            case <= 30:
+                Debug.Log("0.5f");
+
+                //manager.EndMinigame(0.5f);
+                break;
+            default:
+                Debug.Log("-1f");
+
+                //manager.EndMinigame(-1f);
+                break;
+        }
+        //manager.EndMinigame(-1f);
+
+    }
 
     protected override void OnUpdate()
     {
@@ -30,20 +76,6 @@ public class MinigameHeadHit : Minigame
         else if (indicator.anchoredPosition.y > topLimit)
         {
             goingDown = true;
-        }
-
-        if (Input.GetButtonDown("Interact"))
-        {
-
-            float temp = Mathf.Abs(sweetSpot - indicator.anchoredPosition.y);
-            //add audio 
-            
-            //add animation
-            Debug.Log("temp: " + temp + "\nSweetSpot: " + sweetSpot + "\nindicatorPos: " + indicator.anchoredPosition.y);
-
-            //MinigameManager.Instance.EndMinigame(-1f);
-
-
         }
     }
 }
