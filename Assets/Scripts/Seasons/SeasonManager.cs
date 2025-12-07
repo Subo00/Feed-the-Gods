5345168a55584c6519b5bb14c31ebb34bbca0bef
Ernest Subotić <gamedevsubo@gmail.com>
2025-12-07 10:04:20 +0100

    updated SeasonManager adding a list of seasonListeners

--- Assets/Scripts/Seasons/SeasonManager.cs
+++ Assets/Scripts/Seasons/SeasonManager.cs
@@ -1,19 +1,33 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum Season { PLANTING, HARVEST, WINTER }
+public enum Season { SPRING, SUMMER, FALL, WINTER }
+
+public interface ISeasonListener
+{
+    void OnSeasonChanged(Season currentSeason);
+}
+
 public class SeasonManager : MonoBehaviour
 {
     public static SeasonManager Instance;
 
     public event Action OnSeasonChanged;
-    public Season currentSeason = Season.PLANTING;
+    public Season currentSeason = Season.SPRING;
+
+    private List<ISeasonListener> seasonListeners = new List<ISeasonListener> ();
+
+    public void AddSeason(ISeasonListener season) => seasonListeners.Add(season);
+    public void RemoveSeason(ISeasonListener season) => seasonListeners.Remove(season);
 
     public void ChangeSeason()
     {
+        currentSeason = (Season)(((int)currentSeason + 1) % 4);
         Debug.Log(currentSeason);
-        OnSeasonChanged?.Invoke();
+        foreach(var season in seasonListeners)
+            season?.OnSeasonChanged(currentSeason);
     }
 
     private void Awake()
