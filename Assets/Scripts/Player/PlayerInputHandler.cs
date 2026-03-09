fc4990d052ee707ca7e658e7e5f484a2561b980b
Ernest Subotić <gamedevsubo@gmail.com>
2026-03-09 13:33:39 +0100

    Remoce debug lines from PlayerInputHandler

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -11,7 +11,7 @@ public class PlayerInputHandler : MonoBehaviour
     private List<Action> cancleActions;
     private Action uiSubmitAction;
     
-    public void AddOnCancle(Action action) { if (action != null) cancleActions.Add(action); Debug.Log("Cancle lol"); }
+    public void AddOnCancle(Action action) { if (action != null) cancleActions.Add(action);  }
     public void AddOnUISubmit(Action action) { if (action != null) uiSubmitAction = action; }
 
     public void ChangeActionMap(ActionMap map)
@@ -35,7 +35,6 @@ public class PlayerInputHandler : MonoBehaviour
             foreach (var action in cancleActions)
             {
                 action.Invoke();
-                Debug.Log("ACTION" +  action.ToString());   
             }
             ClearInteract();
         }
