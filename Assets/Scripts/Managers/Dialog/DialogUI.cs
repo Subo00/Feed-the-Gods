7cee06a0b9bcd085f43348e4395ca7cccbec8aa6
Ernest Subotić <gamedevsubo@gmail.com>
2026-05-08 11:24:08 +0200

    Update DialogUI to inherit UIPrompt

--- Assets/Scripts/Managers/Dialog/DialogUI.cs
+++ Assets/Scripts/Managers/Dialog/DialogUI.cs
@@ -1,8 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DialogUI : BaseUI
+public class DialogUI : BaseUI, UIPrompt
 {
-   
+    [SerializeField] private GameObject confirmButton;
+    public void SetPrompt(InputPrompt input)
+    {
+        switch (input)
+        {
+            case InputPrompt.WASD:
+                confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmWASD;
+                break;
+            case InputPrompt.IJKL:
+                confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmIJKL;
+                break;
+            case InputPrompt.XBOX:
+                confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmXBOX;
+                break;
+        }
+    }
 }
