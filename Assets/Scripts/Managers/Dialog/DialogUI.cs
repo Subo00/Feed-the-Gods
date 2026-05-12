35d569e0d62e35fd2ff8f15096bb021767293c83
Ernest Subotić <gamedevsubo@gmail.com>
2026-05-12 08:24:05 +0200

    Update DialogCanvas and DIalogUI

--- Assets/Scripts/Managers/Dialog/DialogUI.cs
+++ Assets/Scripts/Managers/Dialog/DialogUI.cs
@@ -4,21 +4,26 @@ using UnityEngine.UI;
 public class DialogUI : BaseUI, UIPrompt
 {
     [SerializeField] private GameObject confirmButton;
+    [SerializeField] private GameObject backButton;
     public void SetPrompt(InputPrompt input)
     {
         switch (input)
         {
             case InputPrompt.WASD:
                 confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmWASD;
+                backButton.GetComponent<Image>().sprite = UIPromptManager.instance.CancelWASD;
                 break;
             case InputPrompt.IJKL:
                 confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmIJKL;
+                backButton.GetComponent<Image>().sprite = UIPromptManager.instance.CancelIJKL;
                 break;
             case InputPrompt.XBOX:
                 confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmXBOX;
+                backButton.GetComponent<Image>().sprite = UIPromptManager.instance.CancelXBOX;
                 break;
             case InputPrompt.PLAYSTATION:
                 confirmButton.GetComponent<Image>().sprite = UIPromptManager.instance.ConfirmPS;
+                backButton.GetComponent<Image>().sprite = UIPromptManager.instance.CancelPS;
                 break;
         }
     }
